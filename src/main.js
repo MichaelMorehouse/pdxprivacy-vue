f2cad08392f0b7e487c329f0373cd7be5458c95d
mmdevelops <mmdevelops@gmail.com>
2019-04-04 15:14:26 -0700

    add vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import About from './components/pages/About'
+import Efforts from './components/pages/Efforts'
+
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: About },
+  { path: '/efforts', component: Efforts }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes // short for `routes: routes`
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
