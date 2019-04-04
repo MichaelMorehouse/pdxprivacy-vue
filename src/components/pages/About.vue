a9c86e80cc930e99d5018437e22ff87e16285ebd
mmdevelops <mmdevelops@gmail.com>
2019-04-04 15:14:19 -0700

    add page content to about and efforts

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,13 +1,50 @@
 <template>
-	
+  <div id="home-content" class="page-content">
+    <div class="section-header">Who we are</div>
+    <p>
+      <span class="leadin-text">W</span>e are a group of people who care about privacy and transparency, and we’re working to protect our data and control its use.
+    </p>
+    <p>We believe that citizens have a right to know:</p>
+    <ul>
+      <li>
+        What technologies are being used to collect data about
+        them
+      </li>
+      <li>The frequency and extent of such data collection</li>
+      <li>How data is stored and how long it is retained</li>
+      <li>
+        What options are available to correct inaccurate
+        information
+      </li>
+      <li>Ways they can opt out of data collection</li>
+      <li>Methods available to have their stored data deleted</li>
+      <li>
+        How much public funding is spent on surveillance
+        equipment
+      </li>
+      <li>
+        The public benefit from the use of surveillance
+        equipment
+      </li>
+    </ul>
+    <p>The public should have input into the acquisition and use of surveillance software and equipment by government and receive reports on:</p>
+    <ul>
+      <li>Usage and effectiveness</li>
+      <li>Data sharing</li>
+      <li>Usage policies and violations thereof</li>
+    </ul>
+    <p>We believe that people have a right to know what data about them is collected and how it’s used. Individuals should have input into the process and maintain control over their data, regardless of whether the information is being gathered by private companies or government agencies. Collected data should only be used for its original purpose unless further consent is granted by the individual.</p>
+    <div class="section-header">Our mission</div>
+    <p>
+      <span class="leadin-text">T</span>o bring about transparency and control in the acquisition and use of surveillance systems in the Portland Metro area, whether personal data is captured by the government or by commercial entities. Transparency is essential to ensure privacy protections, community control, fairness, and respect for civil rights.
+    </p>
+    <p>Check out what initiatives we've been pursuing on our efforts page. Contact us using one of the methods below for more information or to get involved!</p>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- src/components/pages/Efforts.vue
+++ src/components/pages/Efforts.vue
@@ -1,13 +1,137 @@
 <template>
-	
+  <div id="efforts-content" class="page-content">
+    <div class="section-header">Efforts</div>
+    <div class="effort-header">CCOPS</div>
+    <p>
+      <span class="leadin-text">W</span>e’re working to implement
+      the Community Control Over Police Surveillance (CCOPS)
+      principles in the Portland Metro Area. The purpose of the
+      CCOPS is to ensure transparency and public input in the
+      acquisition and use of surveillance systems by law
+      enforcement and to require City Council approval for all
+      purchases of surveillance-related products and services.
+    </p>
+    <p>Our goals:</p>
+    <ul>
+      <li>
+        Implement the CCOPS legislation in the Portland Metro
+        Area
+      </li>
+    </ul>
+    <p>
+      The CCOPS principles are available
+      <a
+        href="https://www.aclu.org/sites/default/files/field_document/ccops_guiding_principles_-_final.pdf"
+        target="_blank"
+      >here</a>.
+    </p>
+    <div class="effort-header">Corporate Surveillance</div>
+    <p>
+      <span class="leadin-text">W</span>e’re working to rein in
+      the collection of user data and its use for purposes other
+      than which it was shared. Most Internet companies today
+      employ an advertising revenue model, where if you’re getting
+      a service for free, you’re usually the product. In exchange
+      for the informative or entertaining content that companies
+      provide to website visitors, they collect personal
+      information about those guests, such as what people read,
+      what they clicked on, or what they bought. Often, this
+      information is then combined with details collected
+      about the users ofﬂine, such as credit card purchases
+      they’ve made.
+    </p>
+    <p>
+      Some physical stores detect WiFi and/or Bluetooth signals
+      emanating from customer mobile phones and use that
+      information to track customers around the store, learning
+      how much time they spent in each department or section of
+      the store.
+    </p>
+    <p>
+      In attempts to make our cities smarter and more
+      efﬁcient, increasing amounts data will be collected in our urban
+      areas. Before putting such systems in place, we need to consider the many ways that our data can
+      be collected and used and must take care in protecting that
+      information.
+    </p>
+    <p>Among other methods, individuals may be tracked via:</p>
+    <ul>
+      <li>Mobile phone GPS and carrier signals</li>
+      <li>CCTV cameras</li>
+      <li>WiFi or Bluetooth signals</li>
+      <li>Sensors in sidewalks and streetlights</li>
+      <li>Credit card purchases</li>
+      <li>Transit passes</li>
+    </ul>
+    <p>
+      While these tools and techniques are not necessarily
+      nefarious, without proper planning and oversight, they could
+      create opportunities for misuse of personal data.
+    </p>
+    <div class="effort-header">Biometric Privacy</div>
+    <p>
+      <span class="leadin-text">W</span>e’re working to get
+      protections in place for biometric data. With the increased
+      use of facial recognition and other systems that record body
+      measurements, there must be safeguards to ensure that
+      this highly personal, difﬁcult to modify data is not misused
+      and is not used without our knowledge and consent.
+    </p>
+    <p>
+      A number of companies and government agencies have grand visions
+      of how biometrics can be used to gather data and to
+      streamline processes.
+    </p>
+    <p>Examples include:</p>
+    <ul>
+      <li>
+        The Department of Homeland Security working on
+        streamlining transit through airports using passenger
+        biometrics
+      </li>
+      <li>
+        Some companies using facial, iris, or ﬁngerprint scans
+        of employees for secure area access or timekeeping
+      </li>
+      <li>
+        Online companies using facial recognition to identify
+        persons in photos
+      </li>
+      <li>
+        Law enforcement agencies using facial recognition to
+        identify persons peacefully exercising their First
+        Amendment rights
+      </li>
+    </ul>
+    <p>
+      While some uses of these technologies may be desirable, a
+      face or ﬁngerprint is not easily changed like a password or
+      credit card number. We need to ensure that individuals have the ability to give consent
+      to having their biometric data being collected and
+      to the ways the collected data will be used before data is amassed.
+    </p>
+    <p>Our goals:</p>
+    <ul>
+      <li>
+        Bring public awareness about the gathering and use of
+        biometric data
+      </li>
+      <li>Advocate for opt-in collection of biometric data</li>
+      <li>
+        Coordinate with other local groups to urge privacy and
+        accountability in policy creation
+      </li>
+      <li>
+        Pursue limits on both government and commercial usage,
+        sharing, and storage of collected biometric data
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
