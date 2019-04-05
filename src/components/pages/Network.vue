455ac4bc365aaff3f3b35944a0320a7d4ad664d5
mmdevelops <mmdevelops@gmail.com>
2019-04-04 18:06:50 -0700

    add content from static site

---
+++ src/components/pages/Network.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="network-content" class="page-content">
+    <div class="subsection-header">Electronic Frontier Alliance</div>
+    <p>
+      PDX Privacy endorses the principles of the
+      <a
+        href="https://www.eff.org/electronic-frontier-alliance"
+        target="_blank"
+      >Electronic Frontier Alliance.</a>
+    </p>We believe that technology should support the intellectual freedom at
+    the heart of a democratic society. In the digital age, that entails advancing:
+    <ol class="principles-list">
+      <li>Free Expression - People should be able to speak their minds to whoever will listen.</li>
+      <li>Security - Technology should be trustworthy and answer to its users.</li>
+      <li>
+        Privacy - Technology should allow private and anonymous speech, and allow users to set their own
+        parameters about what to share with whom.
+      </li>
+      <li>
+        Creativity - Technology should promote progress by allowing people to build on the ideas, creations,
+        and inventions of others.
+      </li>
+      <li>Access to Knowledge - Curiosity should be rewarded, not stifled.</li>
+    </ol>
+    <p>We uphold these principles by fighting for transparency and freedom in culture, code, and law.</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/components/pages/Resources.vue
+++ src/components/pages/Resources.vue
@@ -1,13 +1,73 @@
 <template>
-	
+  <div id="resource-content" class="page-content">
+    <div id="privacy-orgs">
+      <div class="section-header">Privacy organizations</div>
+      <div class="resource-item">
+        <a href="https://www.eff.org" target="_blank">Electronic Frontier Foundation</a>
+        <div>
+          A leading nonprofit organization defending civil
+          liberties in the digital world, based in San
+          Francisco, CA.
+        </div>
+      </div>
+      <div class="resource-item">
+        <a href="https://privacyinternational.org" target="_blank">Privacy International</a>
+        <div>
+          A registered charity based in London that works at
+          the intersection of modern technologies and rights.
+        </div>
+      </div>
+      <div class="resource-item">
+        <a href="https://www.aclu.org" target="_blank">American Civil Liberties Union</a>
+        <div>
+          A nonprofit organization whose stated mission is "to
+          defend and preserve the individual rights and
+          liberties guaranteed to every person in this country
+          by the Constitution and laws of the United States.”
+        </div>
+      </div>
+    </div>
+    <div id="privacy-edu">
+      <div class="section-header">Educational resources</div>
+      <div class="resource-item">
+        <a href="https://defendourmovements.org" target="_blank">Defend our Movements</a>
+        <div>
+          A web-based clearinghouse of the most up-to-date and
+          useful information about protecting your devices and
+          data—whether on the Internet, through cell phone
+          communications, or in your home or office.
+        </div>
+      </div>
+      <div class="resource-item">
+        <a href="https://ssd.eff.org" target="_blank">Surveillance Self Defense</a>
+        <div>
+          An expert guide with Tips, Tools and How-tos for
+          Safer Online Communications to help protect you and
+          your friends from online spying.
+        </div>
+      </div>
+      <div class="resource-item">
+        <a href="https://www.tacticaltech.org" target="_blank">Tactical Technology Collective</a>
+        <div>
+          A Berlin-based non-profit organization working at
+          the intersection of technology, human rights and
+          civil liberties.
+        </div>
+      </div>
+      <div class="resource-item">
+        <a href="https://sec.eff.org" target="_blank">Security Education Companion</a>
+        <div>
+          A resource for people teaching digital security to
+          their friends and neighbors.
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
