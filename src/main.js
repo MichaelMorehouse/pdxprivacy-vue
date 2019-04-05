4c477b0cf1438af334b192f8d5d4b8b50ff858cf
mmdevelops <mmdevelops@gmail.com>
2019-04-05 11:43:34 -0700

    add vue fontawesome icons

--- src/main.js
+++ src/main.js
@@ -2,25 +2,42 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import About from './components/pages/About'
 import Efforts from './components/pages/Efforts'
 import Resources from './components/pages/Resources'
-import Network from './components/pages/Network'
+import Allies from './components/pages/Allies'
 
 Vue.use(VueRouter)
+
+library.add(faEnvelope, faTwitter)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 const routes = [
   { path: '/', component: About },
   { path: '/efforts', component: Efforts },
   { path: '/resources', component: Resources },
-  { path: '/network', component: Network },
+  { path: '/allies', component: Allies },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: __dirname,
-  routes // short for `routes: routes`
+  // short for `routes: routes`
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 new Vue({
