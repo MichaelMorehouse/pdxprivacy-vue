245b71f91f0d06564fff8db5ad7648ac98d80c09
mmdevelops <mmdevelops@gmail.com>
2019-04-04 18:06:22 -0700

    add navbar and footer content from static site

--- src/components/ui/Footer.vue
+++ src/components/ui/Footer.vue
@@ -1,13 +1,26 @@
 <template>
-	
+  <footer class="secondary-color">
+    <div id="contact">
+      <div class="text-centered text-bold">Contact Us</div>
+      <div id="contact-icons">
+        <a href="mailto:info@pdxprivacy.org" target="_blank">
+          <i class="far fa-envelope fa-3x"></i>
+        </a>
+        <a href="https://twitter.com/pdxprivacy" target="_blank">
+          <i class="fab fa-twitter fa-3x"></i>
+        </a>
+      </div>
+      <div>PDX Privacy</div>
+      <div>P.O. Box 3431</div>
+      <div>Portland, OR 97208</div>
+    </div>
+    <div class="copyright">&copy; PDXPrivacy 2019</div>
+  </footer>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- src/components/ui/Navbar.vue
+++ src/components/ui/Navbar.vue
@@ -1,13 +1,129 @@
 <template>
-	
+  <div id="navbar" class="secondary-color">
+    <div class="dropdown">
+      <button class="nav-item">
+        <div class="menu-icon">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="dropbtn-text">PDXPrivacy</div>
+      </button>
+      <div class="dropdown-content">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/efforts">Efforts</router-link>
+        <router-link to="/network">Network</router-link>
+      </div>
+    </div>
+    <div class="nav-item nav-item-right">
+      <a href="#contact">Contact</a>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+/* Navbar styles */
 
+#navbar {
+  display: flex;
+  position: sticky;
+  top: 0;
+}
+
+.nav-item {
+  display: inline-block;
+  background-color: transparent;
+  color: white;
+  padding: 0.8em;
+  font-size: 1.3em;
+  font-weight: 550;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  font-family: inherit;
+  border-right: 1px solid rgb(255, 255, 255, 0);
+}
+
+.dropdown:hover .nav-item,
+.nav-item:not(.nav-item-right):hover {
+  border-right: 1px solid rgb(255, 255, 255, 0.5);
+}
+
+.nav-item-right {
+  margin-left: auto;
+  border-right: none;
+  border-left: 1px solid rgb(255, 255, 255, 0);
+}
+
+.nav-item-right:hover {
+  border-left: 1px solid rgb(255, 255, 255, 0.5);
+}
+
+.nav-item > a {
+  text-decoration: none;
+  color: white;
+  display: block;
+}
+
+.dropbtn-text {
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  width: 100%;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+@media only screen and (max-width: 340px) {
+  .dropbtn-text {
+    display: none;
+  }
+
+  .dropdown-content {
+    width: fit-content;
+  }
+}
+
+.dropdown-content a {
+  color: rgb(20, 30, 123);
+  padding: 0.8em 1em;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {
+  background-color: #f1f1f1;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.menu-icon {
+  float: left;
+  margin: 0 10px;
+  height: 100%;
+}
+
+.menu-icon > div {
+  width: 30px;
+  height: 4px;
+  background-color: white;
+  margin: 4px 0;
+}
 </style>
