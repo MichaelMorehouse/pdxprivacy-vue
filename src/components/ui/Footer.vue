9161d647375976d566321978277af53045622ae0
mmdevelops <mmdevelops@gmail.com>
2019-04-05 10:21:00 -0700

    add js date; icon placeholders

--- src/components/ui/Footer.vue
+++ src/components/ui/Footer.vue
@@ -4,22 +4,29 @@
       <div class="text-centered text-bold">Contact Us</div>
       <div id="contact-icons">
         <a href="mailto:info@pdxprivacy.org" target="_blank">
-          <i class="far fa-envelope fa-3x"></i>
+          <div>ENVELOPE ICON</div>
         </a>
         <a href="https://twitter.com/pdxprivacy" target="_blank">
-          <i class="fab fa-twitter fa-3x"></i>
+          <div>TWITTER ICON</div>
         </a>
       </div>
       <div>PDX Privacy</div>
       <div>P.O. Box 3431</div>
       <div>Portland, OR 97208</div>
     </div>
-    <div class="copyright">&copy; PDXPrivacy 2019</div>
+    <div class="copyright">&copy; PDXPrivacy {{ year }}</div>
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    let date = new Date();
+    return {
+      year: date.getFullYear()
+    };
+  }
+};
 </script>
 
 <style>
