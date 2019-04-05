af3d438bc12c785b4e092947e2d0419866c2db2e
mmdevelops <mmdevelops@gmail.com>
2019-04-05 11:42:27 -0700

    add fontawesome icons

--- src/components/ui/Footer.vue
+++ src/components/ui/Footer.vue
@@ -1,18 +1,26 @@
 <template>
   <footer class="secondary-color">
-    <div id="contact">
-      <div class="text-centered text-bold">Contact Us</div>
-      <div id="contact-icons">
-        <a href="mailto:info@pdxprivacy.org" target="_blank">
-          <div>ENVELOPE ICON</div>
-        </a>
-        <a href="https://twitter.com/pdxprivacy" target="_blank">
-          <div>TWITTER ICON</div>
-        </a>
+    <div class="footer-flex">
+      <div id="contact">
+        <div id="contact-icons">
+          <a href="mailto:info@pdxprivacy.org" target="_blank">
+            <font-awesome-icon :icon="['far','envelope']" size="3x"></font-awesome-icon>
+          </a>
+          <a href="https://twitter.com/pdxprivacy" target="_blank">
+            <font-awesome-icon :icon="['fab','twitter']" size="3x"></font-awesome-icon>
+          </a>
+        </div>
+        <div>PDX Privacy</div>
+        <div>P.O. Box 3431</div>
+        <div>Portland, OR 97208</div>
       </div>
-      <div>PDX Privacy</div>
-      <div>P.O. Box 3431</div>
-      <div>Portland, OR 97208</div>
+      <div>
+        <div>PDX Privacy endorses the principles</div>
+        <div>
+          of the
+          <router-link to="/allies">Electronic Frontier Alliance</router-link>
+        </div>
+      </div>
     </div>
     <div class="copyright">&copy; PDXPrivacy {{ year }}</div>
   </footer>
@@ -30,4 +38,39 @@ export default {
 </script>
 
 <style>
+footer {
+  position: relative;
+}
+
+.footer-flex {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.footer-flex > div {
+  padding: 10px;
+  margin: 10px;
+}
+
+.copyright {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  font-size: 0.7em;
+  margin: 10px;
+}
+
+a {
+  color: white;
+}
+
+#contact-icons {
+  display: flex;
+  justify-content: space-around;
+}
+
+#contact-icons > a {
+  margin: 0 20px 15px;
+}
 </style>
